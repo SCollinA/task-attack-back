25ae5a27c74ac0abbcce307b0352450caeefae01
Collin Argo <collinargo@gmail.com>
2019-02-11 11:18:20 -0500

    add crud routes for users and tasks

--- index.js
+++ index.js
@@ -20,4 +20,31 @@ app.use(bodyParser.urlencoded({extended: false}))
 
 app.use(bodyParser.json())
 
+// CREATE
+// register | add user
+app.post('signup', (req, res) => {
+    res.send('you trying to sign up, chief?')
+})
+// add task
+app.post('addTask', (req, res) => {
+    res.send('you tryina add a task, chief?')
+})
+// RETRIEVE
+// login | get user
+app.post('login', (req, res) => {
+    res.send('you trying to log in, chief?')
+})
+// UPDATE
+app.post('updateUser', (req, res) => {
+    res.send('you trying to update yourself, chief?')
+})
+app.post('updateTask', (req, res) => {
+    res.send('you trying to update a task, chief?')
+})
+// DELETE
+app.delete('deleteTask', (req, res) => {
+    res.send('you trying to delete a task, chief?')
+})
+
+
 app.listen(port, () => console.log(`task-attack-back listening on port ${port}`)) 
