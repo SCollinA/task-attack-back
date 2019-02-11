0c61c0dffdcd918cc43af7ce4dd2c8625959d2fd
Collin Argo <collinargo@gmail.com>
2019-02-11 08:53:48 -0500

    add dependencies, set up express

---
+++ index.js
@@ -0,0 +1,22 @@
+const express = require('express')
+const session = require('express-session')
+const store = new (require('connect-pg-simple')(session))({ pgPromise: db })
+const app = express()
+const port = 7000
+const db = require('./db')
+
+app.use(session({
+    store,
+    secret: 'sUpErS3cR3t',
+    resave: false,
+    saveUninitialized: false,
+    cookie: { maxAge: 1 * 24 * 60 * 60 * 1000 } // 1 days
+}));
+
+app.use(express.static('public'))
+
+app.use(bodyParser.urlencoded({extended: false}))
+
+app.use(bodyParser.json())
+
+app.listen(port, () => console.log(`task-attack-back listening on port ${port}`))
